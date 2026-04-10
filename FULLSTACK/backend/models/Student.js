71e9b6f0959f5ce7036d4659745abe7099988cf6
Akshaya Kalidoss <akshayakalidoss01@gmail.com>
2026-04-10 15:24:53 +0530

    Delete FULLSTACK directory

--- FULLSTACK/backend/models/Student.js
+++
@@ -1,9 +0,0 @@
-
-const mongoose = require('mongoose');
-const studentSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  department: { type: String, required: true },
-  year: { type: Number, required: true },
-  email: { type: String, required: true }
-});
-module.exports = mongoose.model('Student', studentSchema);
